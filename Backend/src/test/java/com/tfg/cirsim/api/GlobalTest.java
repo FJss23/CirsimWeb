0d3f95b29941d5893780988fd0ee9f008c4eb3a2
FJss23 <uo252473@uniovi.es>
2019-07-07 20:43:37 +0200

    Add: finished test

--- Backend/src/test/java/com/tfg/cirsim/api/GlobalTest.java
+++ Backend/src/test/java/com/tfg/cirsim/api/GlobalTest.java
@@ -3,23 +3,25 @@ package com.tfg.cirsim.api;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.junit.jupiter.api.Test;
+import org.assertj.core.util.Lists;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tfg.cirsim.api.entities.Exercise;
 import com.tfg.cirsim.api.entities.Role;
 import com.tfg.cirsim.api.entities.Task;
 import com.tfg.cirsim.api.entities.User;
 import com.tfg.cirsim.api.repository.TaskRepository;
+import com.tfg.cirsim.api.repository.UserRepository;
 
 /**
  * 
@@ -28,50 +30,54 @@ import com.tfg.cirsim.api.repository.TaskRepository;
  */
 @RunWith(SpringRunner.class)
 @DataJpaTest
-public class TaskTest {
-	
+public class GlobalTest {
+ 
     @Autowired
     private TestEntityManager entityManager;
  
     @Autowired
+    private UserRepository userRepository;
+    
+    @Autowired
     private TaskRepository taskRepository;
     
     @Test
-    public void whenFindAll_thenReturnAll() {
-        Task task = new Task(1L, "Título test", new Date() , new HashSet<Exercise>());
-        entityManager.persist(task);
+    public void whenFindByRole_thenReturnUser() {
+        User teacher = new User("user3","123","Esteban","Hernandez", Role.TEACHER);
+        entityManager.persist(teacher);
         entityManager.flush();
-        
-        Set<Task> founded = new HashSet<Task>();
-        taskRepository.findAll().forEach(founded::add);
+        Set<User> found = userRepository.findByRole(Role.TEACHER);
      
-        assertThat(founded.size())
+        assertThat(found.size())
           .isEqualTo(1);
+        
+        found.forEach(user -> {
+            assertThat(user.getRole())
+            .isEqualTo(Role.TEACHER);
+        });
+
     }
     
     @Test
-    public void whenDeleteAll_thenNoReturn() {
-    	Task task = new Task(1L, "Título test", new Date() , new HashSet<Exercise>());
-        entityManager.persist(task);
-        Task secondTask = new Task(2L, "Título test2", new Date() , new HashSet<Exercise>());
-        entityManager.persist(secondTask);
+    public void whenFindAllUsers_thenReturnAll() {
+        User student = new User("user2","123","Juan","Juanito", Role.STUDENT);
+        entityManager.persist(student);
         entityManager.flush();
         
-        taskRepository.deleteAll();
-        List<Task> contList = new ArrayList<Task>();
-        taskRepository.findAll().forEach(contList::add);
+        Iterable<User> found = userRepository.findAll();
+        List<User> contList = new ArrayList<User>();
+        found.forEach(contList::add);
         
         assertThat(contList.size())
-        .isEqualTo(0);
+        .isEqualTo(1);
     }
     
- /*   @Test
-    public void whenFindOnewithExercise_thenReturnOne() {
-    	Set<Exercise> exercises = new HashSet<Exercise>();
-    	exercises.add(new Exercise(1L, "", description, orderEx, connections, points, image));
-    	
-    	Task task = new Task(1L, "Título test", new Date(), exercises);
-        entityManager.persist(task);
+    @Test
+    public void whenDeleteAllExceptAdmin_thenReturnAdmin() {
+        User admin = new User("user1","123","Pedro","Hernandez", Role.ADMIN);
+        entityManager.persist(admin);
+        User teacher = new User("user3","123","Esteban","Hernandez", Role.TEACHER);
+        entityManager.persist(teacher);
         entityManager.flush();
         
         userRepository.deleteAllExceptAdmin();
@@ -131,5 +137,57 @@ public class TaskTest {
         
         assertThat(contList.size())
         .isEqualTo(0);
-    }*/
+    }
+
+    @Test
+	@Transactional
+    public void whenFindAll_thenReturnAll() {
+        Task task = new Task();
+        entityManager.persist(task);
+        entityManager.flush();
+        
+        Set<Task> founded = new HashSet<Task>();
+        taskRepository.findAll().forEach(founded::add);
+     
+        assertThat(founded.size())
+          .isEqualTo(1);
+    }
+    
+    @Test
+    public void whenDeleteAll_thenNoReturn() {
+    	Task task = new Task();
+        entityManager.persist(task);
+        Task secondTask = new Task();
+        entityManager.persist(secondTask);
+        entityManager.flush();
+        
+        taskRepository.deleteAll();
+        List<Task> contList = new ArrayList<Task>();
+        taskRepository.findAll().forEach(contList::add);
+        
+        assertThat(contList.size())
+        .isEqualTo(0);
+    }
+    
+    @Test
+    public void whenFindOnewithExercise_thenReturnOne() {
+    	String taskTitle = "Título test";
+    	Set<Exercise> exercises = new HashSet<Exercise>();
+    	Exercise ex = new Exercise();
+    	exercises.add(ex);
+    	
+    	Task task = new Task();
+    	task.setTitle(taskTitle);
+        task.setExercises(exercises);
+        exercises.forEach(exer -> exer.setTask(task));
+        entityManager.persist(ex);
+        entityManager.persist(task);
+        entityManager.flush();
+
+        Iterable<Task> founded = taskRepository.findAll();
+    
+        
+        assertThat(Lists.newArrayList(founded).size())
+        .isEqualTo(1);
+    }
 }
